cc3446710fa3a2a14fd112d94dee882d76d78c12
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-08-15 13:47:33 -0700

    Create fastq2psmcfa.sh

---
+++ DemographicHistory/fastq2psmcfa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH --job-name=fq2psmcfa
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 12:00:00
+#SBATCH --mem=10GB
+#SBATCH -p high
+#SBATCH -o fq2psmcfa.out
+#SBATCH -e fq2psmcfa.err
+
+/home/hennelly/bin/psmc/utils/fq2psmcfa ../../SanClemente_2009_subset.fq.gz > ../../PSMC_files_new/SanClemente_2009_autosomes.psmc.fa
+/home/hennelly/bin/psmc/utils/fq2psmcfa ../../SanMiguel_2008_subset.fq.gz > ../../PSMC_files_new/SanMiguel_2008_autosomes.psmc.fa
+/home/hennelly/bin/psmc/utils/fq2psmcfa ../../SanNicolas_1929_SE_subset.fq.gz > ~../../PSMC_files_new/SanNicolas_1929_SE_autosomes.psmc.fa
+...
+...
+...
+
+## Note these don't take very long so I ran them in sequence instead of making an array
