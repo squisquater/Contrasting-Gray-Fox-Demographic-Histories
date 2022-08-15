ba6f3dd33ce6c6f5b80a2af4c48812fc62257032
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2022-08-15 13:50:01 -0700

    Update bam2fq_autosomes.sh

--- DemographicHistory/bam2fq_autosomes.sh
+++ DemographicHistory/bam2fq_autosomes.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -l
 #SBATCH --job-name=bam2fq_autosomes
-#SBATCH --array=1-16
+#SBATCH --array=1-n
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --time 20:00:00
 #SBATCH --mem=4GB
 #SBATCH -p high
-#SBATCH -o /home/sophiepq/GrayFoxWGS/GrayFox1/slurmlogs/bam2fq_autosomes%A_%a.out
-#SBATCH -e /home/sophiepq/GrayFoxWGS/GrayFox1/slurmlogs/bam2fq_autosomes%A_%a.err
+#SBATCH -o bam2fq_autosomes%A_%a.out
+#SBATCH -e bam2fq_autosomes%A_%a.err
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
-SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ~/GrayFoxWGS/GrayFox1/mlRho/bam_subset_2/bamlist_depth.txt | cut -f1)
-BAM=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ~/GrayFoxWGS/GrayFox1/mlRho/bam_subset_2/bamlist_depth.txt | cut -f2)
-MIN_DEPTH=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ~/GrayFoxWGS/GrayFox1/mlRho/bam_subset_2/bamlist_depth.txt | cut -f4)
-MAX_DEPTH=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ~/GrayFoxWGS/GrayFox1/mlRho/bam_subset_2/bamlist_depth.txt | cut -f5)
+SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" bamlist_depth.txt | cut -f1)
+BAM=$(sed "${SLURM_ARRAY_TASK_ID}q;d" bamlist_depth.txt | cut -f2)
+MIN_DEPTH=$(sed "${SLURM_ARRAY_TASK_ID}q;d" bamlist_depth.txt | cut -f4)
+MAX_DEPTH=$(sed "${SLURM_ARRAY_TASK_ID}q;d" bamlist_depth.txt | cut -f5)
 
 module load htslib
 module load samtools
@@ -21,7 +21,7 @@ module load bcftools
 
 samtools mpileup -Q 30 -q 30 -u -v -f ~/Reference_Genomes/canFam3_withY.fa ${BAM} |
 bcftools call -c |  
-vcfutils.pl vcf2fq -d ${MIN_DEPTH} -D ${MAX_DEPTH} -Q 30 | gzip > ~/GrayFoxWGS/GrayFox1/angsd/PSMC/final_fqfiles/${SAMPLE}.fq.gz
+vcfutils.pl vcf2fq -d ${MIN_DEPTH} -D ${MAX_DEPTH} -Q 30 | gzip > ../../PSMC/final_fqfiles/${SAMPLE}.fq.gz
 
 
 ##bamlist_depth.txt is a file with 5 columns where:
